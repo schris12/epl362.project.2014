1d4fff53d3f4998dd930ba9428ad41d0bbb382d5
cchara11 <cchara11@cs.ucy.ac.cy>
2014-04-30 17:43:11 +0300

    add lawyer

---
+++ epl362TeamProject/src/webservices/PutLawyer.java
@@ -0,0 +1,35 @@
+package webservices;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class PutLawyer {
+	public int put_lawyer(String id, String name, String surname) {
+		String dbUrl = "jdbc:mysql://localhost/362";
+		try {
+
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con = DriverManager.getConnection(dbUrl, "root","");
+
+			String insertTableSQL = "INSERT INTO lawyer"
+			+ "(lawyer, name, surname) VALUES"
+			+ "(?,?,?)";
+			java.sql.PreparedStatement preparedStatement = con.prepareStatement(insertTableSQL);
+			preparedStatement.setString(1, id);
+			preparedStatement.setString(1, name);
+			preparedStatement.setString(2, surname);
+			int ret = preparedStatement.executeUpdate();
+			
+			con.close();
+			return ret;		
+		}
+		catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		catch (SQLException e) {			
+			e.printStackTrace();
+		}
+		return -1;
+	}
+}
