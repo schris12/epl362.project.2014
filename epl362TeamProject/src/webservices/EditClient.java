7d9c373cac493453353727ccdbec451dc04d9c34
Stefanos Chrs <schris12@cs.ucy.ac.cy>
2014-04-30 18:00:22 +0300

    edit client

---
+++ epl362TeamProject/src/webservices/EditClient.java
@@ -0,0 +1,34 @@
+package webservices;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class EditClient {
+	public int edit_client(String id, String name, String surname) {
+		String dbUrl = "jdbc:mysql://localhost/362";
+		try {
+
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con = DriverManager.getConnection(dbUrl, "root","");
+
+			String insertTableSQL = "UPDATE client"
+			+ " SET (name=?, surname=?) WHERE (client_id=?)";
+			java.sql.PreparedStatement preparedStatement = con.prepareStatement(insertTableSQL);
+			preparedStatement.setString(1, id);
+			preparedStatement.setString(2, name);
+			preparedStatement.setString(3, surname);
+			int ret = preparedStatement.executeUpdate();
+			
+			con.close();
+			return ret;		
+		}
+		catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		catch (SQLException e) {			
+			e.printStackTrace();
+		}
+		return -1;
+	}
+}
