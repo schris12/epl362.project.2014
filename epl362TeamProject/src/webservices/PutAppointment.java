8a17baed34840c8b891b4af28adf5b95cbcd2c56
cchara11 <cchara11@cs.ucy.ac.cy>
2014-04-30 18:10:44 +0300

    put appointment

--- epl362TeamProject/src/webservices/PutAppointment.java
+++ epl362TeamProject/src/webservices/PutAppointment.java
@@ -2,38 +2,39 @@ package webservices;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class PutAppointment {
-	public int put_appointment(String client_id, String lawyer_id, String date,String branch_id) {
-		int i=0;
+	public int put_appointment(String client_id, String lawyer_id, String date, String branch_id) throws ParseException {
 		String dbUrl = "jdbc:mysql://localhost/362";
 		try {
 
 			Class.forName("com.mysql.jdbc.Driver");
 			Connection con = DriverManager.getConnection(dbUrl, "root","");
 
-			boolean ret=con.prepareStatement("INSERT INTO `client`(`ID`) VALUES (1111)").execute();
-//			String insertTableSQL = "INSERT INTO appointments"
-//					+ "(client_id) VALUES"
-//					+ "(?)";
-//			java.sql.PreparedStatement preparedStatement = con.prepareStatement(insertTableSQL);
-//			preparedStatement.setString(1, "test");
-//			int ret =preparedStatement .executeUpdate();
+			String insertTableSQL = "INSERT INTO appointments"
+			+ "(client_id, lawyer_id, date, missed, branch_id) VALUES"
+			+ "(?,?,?,?,?)";
+			java.sql.PreparedStatement preparedStatement = con.prepareStatement(insertTableSQL);
+			preparedStatement.setString(1, client_id);
+			preparedStatement.setString(2, lawyer_id);
+			java.util.Date utilDate = new SimpleDateFormat("yyyy-MM-dd").parse(date);
+			java.sql.Date sqlDate = new java.sql.Date(utilDate.getTime());
+			preparedStatement.setDate(3, sqlDate);
+			preparedStatement.setString(5, branch_id);
+			int ret = preparedStatement.executeUpdate();
 			
 			con.close();
-			return 1;		
-		} // end try
+			return ret;		
+		}
 		catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		}
 		catch (SQLException e) {			
 			e.printStackTrace();
-			return -1;
 		}
-		return 2;
+		return -1;
 	}
 }
